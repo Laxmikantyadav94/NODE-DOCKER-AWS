e0e9a05fbf5dc2530b760cbbe92277f8e7dfe9ca
Laxmikant <laxmikantm@smartek21.com>
2021-01-09 16:25:01 +0530

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app =express();
 const router = express.Router();
 
-
+router.get('/',(req,res,next)=>{
+    res.send("working");
+})
 router.get('/user',(req,res,next)=>{
     res.send("this is user response");
 })
